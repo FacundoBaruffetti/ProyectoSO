9d520273ed544cc311a6188fad6dc3fe85d1e6f5
Facundo Baruffetti <facubaru4@gmail.com>
2024-10-04 08:04:31 -0300

    inicializacion archivos Santa Claus2

---
+++ Santa Claus/Santa.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define REPETITIONS 100  // Número de repeticiones
+
+// Declaración de semáforos
+
+
+
+int main{
+    //Inicializacion de los semaforos
+
+    //Inicializacion de los mutex
+
+    // Creación de los hilos
+
+    // Espera a que los hilos terminen
+
+    // Destrucción de los semáforos
+
+    printf('\n');
+    return 0;
+}
---
+++ Santa Claus/execute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc Santa.c -o Santa
+
+./Santa
